98ee1bd8f7eaebea0b6b829906b50cf31aac8c32
Ashkan Aazami <aazami@gmail.com>
2020-02-13 13:41:41 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ from setuptools import setup
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
 requirements = [
     # TODO: put package requirements here
 ]
